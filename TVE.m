50c8c89d1fc8a79deffd9b20c70f19c35c730aab
Jorge Caballero <jorge@2build.it>
2015-12-13 19:02:19 -0600

    ultimos cambios a tve

--- TVE.m
+++ TVE.m
@@ -11,5 +11,17 @@ p0 = (a+b)/2;
 
 sol = NR(f,p0,30);
 
+if sol > a && sol < b
+    disp('p pertenece al conjunto [a,b]');
+    bis = Biseccion(f,a,b,0.0001);
+    disp(sort(bis));
+end
+disp('NR = ');
+disp(sol);
+
+min = fminbnd(inline(f),a,b);
+maxi = max(f);
+
+disp(maxi);
 end
 
